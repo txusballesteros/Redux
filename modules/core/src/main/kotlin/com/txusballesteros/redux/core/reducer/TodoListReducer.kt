bb25487019500f632fce91e0979f1077ae34d07e
Txus Ballesteros <j.ballesteros@mytaxi.com>
2018-01-09 12:49:55 +0100

    Improving Reducer Readability

--- modules/core/src/main/kotlin/com/txusballesteros/redux/core/reducer/TodoListReducer.kt
+++ modules/core/src/main/kotlin/com/txusballesteros/redux/core/reducer/TodoListReducer.kt
@@ -1,20 +1,20 @@
 package com.txusballesteros.redux.core.reducer
 
-import com.txusballesteros.redux.reduxcore.Action
-import com.txusballesteros.redux.reduxcore.Reducer
 import com.txusballesteros.redux.core.action.AddTodoAction
 import com.txusballesteros.redux.core.state.TodoListState
-import com.txusballesteros.redux.core.state.TodoState
+import com.txusballesteros.redux.reduxcore.Action
+import com.txusballesteros.redux.reduxcore.Reducer
 
 class TodoListReducer: Reducer<TodoListState> {
     override fun reduce(action: Action, oldState: TodoListState): TodoListState =
         when (action) {
-            is AddTodoAction -> {
-                with(oldState.list.toMutableList()) {
-                    add(TodoState(action.text))
-                    TodoListState(this)
-                }
-            }
+            is AddTodoAction -> addNewToDo(oldState, action)
             else -> oldState
+        }
+
+    private fun addNewToDo(currentState: TodoListState, action: AddTodoAction): TodoListState
+        = with(currentState.list.toMutableList()) {
+            add(com.txusballesteros.redux.core.state.TodoState(action.text))
+            com.txusballesteros.redux.core.state.TodoListState(this)
         }
 }
